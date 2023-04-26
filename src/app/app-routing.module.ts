8391ec659e77226b3f93cbeffa98ffe4cf481d64
Carmen Arias <maricarmen98a@gmail.com>
2023-04-26 19:26:13 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './components/intro/intro.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IntroComponent },
+  { path: 'login', component: LoginComponent },
+
+
+  { path: '**', redirectTo: '' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
