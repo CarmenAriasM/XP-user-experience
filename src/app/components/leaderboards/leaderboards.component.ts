a9bff4b72e018f5717193cddc444b0b951aa4fbd
Carmen Arias <maricarmen98a@gmail.com>
2023-05-11 21:43:39 +0200

    Finished leaderboard, friends leaderboard yet to be finished

--- src/app/components/leaderboards/leaderboards.component.ts
+++ src/app/components/leaderboards/leaderboards.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class LeaderboardsComponent {
   leaderboards: boolean = true;
+  showLunchLeaderboard: boolean = false;
+  showOnlineSalesLeaderboard: boolean = false;
+  showLeaderboard(leaderboard: string) {
+    let arrow = document.getElementById(leaderboard + '-arrow') as HTMLElement | null;
+    if(arrow?.classList.contains('rotated')) {
+      arrow.classList.remove('rotated')
+    } else {
+      arrow!.classList.add('rotated')
+    }
+    if(leaderboard == 'lunch') {
+      this.showLunchLeaderboard = !this.showLunchLeaderboard;
+    }
+    if(leaderboard == 'online-sales') {
+      this.showOnlineSalesLeaderboard = !this.showOnlineSalesLeaderboard;
+    }
+  }
 }
