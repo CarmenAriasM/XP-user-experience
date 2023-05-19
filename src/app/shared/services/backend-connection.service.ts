8b4aafa37cd5961ff0b309882ff9b0f2a12b017f
Carmen Arias <maricarmen98a@gmail.com>
2023-05-19 13:48:39 +0200

    added https

--- src/app/shared/services/backend-connection.service.ts
+++ src/app/shared/services/backend-connection.service.ts
@@ -5,7 +5,7 @@ import { Observable, catchError, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class BackendConnectionService {
-  apiUrl = 'http://34.147.140.133:80/';
+  apiUrl = 'https://34.147.140.133:80/';
   constructor(private http: HttpClient) { }
   getData(data: any): Observable<any> {
     return this.http.post(this.apiUrl + 'User/login', data).pipe(catchError(this.handleError));
