a8578d09ac878c545bc6aa5ec48293158338d971
Carmen Arias <maricarmen98a@gmail.com>
2023-05-19 23:05:05 +0200

    changed environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    webapiurl: (window as any)["envconfig"]["apiurl"] || "default"                   
+    webapiurl: (window as any)["envconfig"]["apiurl"] || "http://34.147.140.133:80/"                   
 };
