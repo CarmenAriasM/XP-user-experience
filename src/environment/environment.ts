fe66017bf24c52152a7f21ac13d8583abdb8ad7a
Carmen Arias <maricarmen98a@gmail.com>
2023-05-20 14:54:29 +0200

    changed angularjson

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    webapiurl: (window as any)["envconfig"]["apiurl"] || "https://34.147.140.133:80/"                   
+    webapiurl: "https://34.147.140.133:80/"                   
 };
