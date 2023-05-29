61e51f9ba2146f42780f9c1ade1e05644f56cc5c
dummer3 <65780844+dummer3@users.noreply.github.com>
2023-05-29 17:20:30 +0200

    Update backend-connection.service.ts

--- src/app/shared/services/backend-connection.service.ts
+++ src/app/shared/services/backend-connection.service.ts
@@ -25,8 +25,8 @@ export class BackendConnectionService {
     return this.http.get( this.apiUrl + 'User/get/user').pipe(catchError(this.handleError));  
   } */
   getCollegeLeaderboard(idUniversity : number) : Observable<any> {
-    let params = new URLSearchParams();
-    params.set('id', idUniversity);
+  let params = new URLSearchParams();
+  params.append('id', idUniversity.toString());
     console.log("request")
     return this.http.get( this.apiUrl + 'User/yourUniversityLeaderboard', params).pipe(catchError(this.handleError));  
   }
